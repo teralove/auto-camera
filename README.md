f18e9efc95d0e2646c074f8d1ca469ca2dd5d274
teralove <terascripts@gmail.com>
2017-04-27 04:54:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Camera Distance
+
+Unlocks the 3rd person camera's maximum viewing distance.
+If you want the camera to be like the "Harrowhold camera" all of the time, then set the variable OVERRIDE_DEFAULT_SETTINGS to true and DEFAULT_MAX_DISTANCE to 1200. 
+You can customize the camera's max distance in-game by using the "camera [distance]" command. 
+Requires tera-data files from at least 4-25-2017.
+
+
+## Chat commands:
+camera [distance]	set distance to override camera's current and maximum distance from player
+### Examples:
+"camera 1000"		sets the camera's current and maximum viewing distance to 1000
+"camera 0"			will revert camera to use regular settings on next respawn (only if OVERRIDE_DEFAULT_SETTINGS is false)
+
+
+## Info
+[*] Normal camera's maximum distance is 500
+[*] Harrohold camera's maximum distance is 1200
+[*] By default, the current viewing distance (not max) when you login is 170
---
+++ index.js
@@ -0,0 +1,51 @@
+//vers 1.0
+
+const format = require('./format.js');		//thank you fps-utils...
+
+const OVERRIDE_DEFAULT_SETTINGS = false;
+const DEFAULT_MAX_DISTANCE = 1200;			// Use this distance override on start when OVERRIDE_DEFAULT_SETTINGS = true
+
+module.exports = function CameraDistance(dispatch) {
+	
+	let lastDistance = 0;
+		
+    dispatch.hook('S_SPAWN_ME', 1, function(event) {
+		/*
+		Camera settings get reset everytime the player respawns. Setting the camera distance immediatley on spawns doesn't work.
+		A lame workaround is to delay the dispatch by a second. Might have to increase time delay on slower computers...
+		*/
+		setTimeout(() => {
+			if (OVERRIDE_DEFAULT_SETTINGS && lastDistance == 0) {
+				lastDistance = DEFAULT_MAX_DISTANCE;
+			}
+		
+			// update camera settings if distance has been changed, else keep the original camera behavior.
+			if (lastDistance != 0) {
+				doCameraDispatch(lastDistance);
+			}	
+		}, 1000);
+	})
+	
+	function doCameraDispatch(distance)	{
+		if (isNaN(distance)) return;
+		lastDistance = distance;
+		
+		dispatch.toClient('S_DUNGEON_CAMERA_SET', {
+			enabled: true,
+			default: distance,		// set the current viewing distance
+			max: distance			// set the maximum scrolling range
+		});
+	}
+		
+    dispatch.hook('C_CHAT', 1, function(event) {
+		let command = format.stripTags(event.message).split(' ');
+
+		if(command[0] === 'camera') {
+			if (command.length > 1) {
+				doCameraDispatch(parseInt(command[1]));
+			}
+			return false;			
+		}
+	});	
+	
+}
