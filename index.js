b6f96a53e6c7eef647984d2ed8d47a0bf8942e07
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-10-06 12:03:10 +0900

    Update index.js
    - Personalized code aesthetics
    - Added Pinkie Pie's Command module support
    - Removed Slash support
    - Removed Format support

--- index.js
+++ index.js
@@ -1,59 +1,50 @@
-//vers 1.1
-
-const format = require('./format.js');		//thank you fps-utils...
-
-const OVERRIDE_DEFAULT_SETTINGS = false;
-const DEFAULT_MAX_DISTANCE = 1200;			// Use this distance override on start when OVERRIDE_DEFAULT_SETTINGS = true
+// OPCODE REQUIRED :
+// - S_DUNGEON_CAMERA_SET
+// - S_SPAWN_ME
 
 module.exports = function CameraDistance(dispatch) {
-	
-	let lastDistance = 0;
-		
-    dispatch.hook('S_SPAWN_ME', 1, function(event) {
-		/*
-		Camera settings get reset everytime the player respawns. Setting the camera distance immediatley on spawns doesn't work.
-		A lame workaround is to delay the dispatch by a second. Might have to increase time delay on slower connections...
-		*/
-		setTimeout(() => {
-			if (OVERRIDE_DEFAULT_SETTINGS && lastDistance == 0) {
-				lastDistance = DEFAULT_MAX_DISTANCE;
+
+	let enable = true
+	let defaultDistance = 800
+	let lastDistance = 0
+
+	// command
+	try {
+		const Command = require('command')
+		const command = Command(dispatch)
+		command.add('camera', (distance) => {
+			if (isNaN(distance)) {
+				send(`[camera-distance] : <font color="#FF0000">Invalid argument.</font>`)
+				return
 			}
-		
-			// update camera settings if distance has been changed, else keep the original camera behavior.
-			if (lastDistance != 0) {
-				doCameraDispatch(lastDistance);
-			}	
-		}, 1000);
+			setCamera(distance)
+			send(`[camera-distance] : Distance set at <font color="#56B4E9">${distance}</font>.`)
+		})
+		function send(msg) {
+			command.message(`<font color="#FFFFFF">` + msg + `</font>`)
+		}
+	} catch (e) {
+		console.log(`[ERROR] -- camera-distance module --`)
+	}
+	
+	// code
+    dispatch.hook('S_SPAWN_ME', 1, function(event) {
+		setTimeout(() => {
+			// check on/off and if there is a previous distance set
+			if (enable && lastDistance == 0) { lastDistance = defaultDistance }
+			// maintain previous distance
+			if (lastDistance != 0) { setCamera(lastDistance) }	
+		}, 1000)
 	})
 	
-	function doCameraDispatch(distance)	{
-		if (isNaN(distance)) return;
+	// helper
+	function setCamera(distance) {
 		lastDistance = distance;
-		
 		dispatch.toClient('S_DUNGEON_CAMERA_SET', {
 			enabled: true,
-			default: distance,		// set the current viewing distance
-			max: distance			// set the maximum scrolling range
-		});
+			default: distance,
+			max: distance
+		})
 	}
-		
-    dispatch.hook('C_CHAT', 1, function(event) {
-		let command = format.stripTags(event.message).split(' ');
-
-		if(command[0] === '!camera') {
-			if (command.length > 1) {
-				doCameraDispatch(parseInt(command[1]));
-			}
-			return false;			
-		}
-	});	
 	
-	// slash support, thanks to wuaw for snippet
-	try {
-		const Slash = require('slash')
-		const slash = new Slash(dispatch)
-		slash.on('camera', args => doCameraDispatch(parseInt(args[1])))
-	} catch (e) {
-		// do nothing because slash is optional
-	}	
 }
