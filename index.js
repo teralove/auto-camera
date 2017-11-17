7a8523023dbf9e3039d87fbc0faf534496e2f24a
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-11-17 14:50:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,51 +2,24 @@
 // - S_DUNGEON_CAMERA_SET
 // - S_SPAWN_ME
 
-const Command = require('command')
+const DEFAULT_DISTANCE = 800
 
 module.exports = function CameraDistance(dispatch) {
 
-	const command = Command(dispatch)
+	let enable = true,
+		lastDistance = 0
 
-	let enable = true
-	let defaultDistance = 800
-	let lastDistance = 0
-
-	// command
-	try {
-		command.add('camera', (distance) => {
-			if (distance === undefined) {
-                enable = !enable
-                send(`${enable ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'}<font>.</font>`)
-                return
-            }
-			if (isNaN(distance)) {
-				send(`<font color="#FF0000">Invalid argument.</font>`)
-				return
-			}
-			setCamera(distance)
-			send(`Distance set at <font color="#56B4E9">${distance}</font><font>.</font>`)
-		})
-	} catch (e) {
-		console.log(`[ERROR] -- camera-distance module --`)
-	}
-
-	// command chat function
-	function send(msg) {
-		command.message(`[camera-distance] : ` + msg)
-	}
-	
 	// code
-    dispatch.hook('S_SPAWN_ME', 1, function(event) {
+	dispatch.hook('S_SPAWN_ME', function (event) {
 		if (!enable) return
 		setTimeout(() => {
 			// check on/off and if there is a previous distance set
-			if (enable && lastDistance == 0) { lastDistance = defaultDistance }
+			if (enable && lastDistance == 0) { lastDistance = DEFAULT_DISTANCE }
 			// maintain previous distance
-			if (lastDistance != 0) { setCamera(lastDistance) }	
+			if (lastDistance != 0) { setCamera(lastDistance) }
 		}, 1000)
 	})
-	
+
 	// helper
 	function setCamera(distance) {
 		lastDistance = distance
@@ -56,5 +29,29 @@ module.exports = function CameraDistance(dispatch) {
 			max: distance
 		})
 	}
-	
+
+	// command
+	try {
+		const Command = require('command')
+		const command = Command(dispatch)
+		command.add('camera', (distance) => {
+			if (distance === undefined) {
+				enable = !enable
+				send(`${enable ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'}<font>.</font>`)
+				return
+			}
+			if (isNaN(distance)) {
+				send(`<font color="#FF0000">Invalid argument.</font>`)
+				return
+			}
+			setCamera(distance)
+			send(`Distance set at <font color="#56B4E9">${distance}</font><font>.</font>`)
+		})
+		function send(msg) {
+			command.message(`[camera-distance] : ` + msg)
+		}
+	} catch (e) {
+		console.log(`[ERROR] -- camera-distance module --`)
+	}
+
 }
