ea59e89185c50abdf26a80747cff036d726b286d
teralove <terascripts@gmail.com>
2018-12-11 05:57:25 -0500

    Next Gen Update

--- README.md
+++ README.md
@@ -1,63 +1,31 @@
 # auto-camera
-tera-proxy module to unlock maximum viewing distance
+Unlock maximum viewing distance
 
-## Dependency
-- `Command` module
-
-## Usage
-- __`camera` · `cam`__
+## Commands
+- __`cam`__
   - Toggle on/off
-- __`camera num` · `cam num`__
-  - Set distance at `num`, where `num` is a value between `0` and `99999`
+- __`cam num`__
+  - Set distance at `num`, where `num` is a value between `0` and `inf`
+### Arguments
+- __`cam add num`__
+  - Set character-specific distance at `num`, where `num` is a value between `0` and `inf`
+- __`cam rm`__
+  - Remove character-specific distance setting
+- __`cam set num`__
+  - Set default distance at `num`, where `num` is a value between `0` and `inf`
 
 ## Config
 - __`enable`__
   - Initialize module on/off
-  - Default is on
+  - Default is `true`
 - __`defaultDistance`__
   - Initialize camera distance
   - Default distance is `800`
 
 ## Info
-- Original author : [teralove](https://github.com/teralove)
 - Normal camera maximum distance is 500
 - Harrowhold camera maximum distance is 1200
 - By default, the initial viewing distance (not max) when you login is 170
-
-## Changelog
-<details>
-
-    1.46
-    - Added auto-update support
-    - Refactored config file
-    -- Added `enable`
-    -- Added `defaultDistance`
-    1.45
-    - Updated name and font color
-    1.44
-    - Updated code aesthetics
-    1.43
-    - Updated code
-    - Added string function
-    1.42
-    - Updated code aesthetics
-    1.41
-    - Updated code aesthetics
-    1.31
-    - Updated code
-    1.30
-    - Updated code
-    - Removed protocol version restriction
-    1.21
-    - Added Command dependency
-    - Removed slash support
-    1.20
-    - Initial Fork
-    1.1.0
-    - Changed command to require exclamation prefix '!'
-    - Added slash support
-
-</details>
 
 ---
 ![Screenshot](http://i.imgur.com/LzxGSgm.jpg)
--- index.js
+++ index.js
@@ -1,46 +1,108 @@
-// Version 1.46 r:07
+'use strict';
 
-const Command = require('command')
-const config = require('./config.json')
+module.exports = function AutoCamera(mod) {
+    const fs = require('fs');
+    const path = require('path');
 
-// credit : https://github.com/Some-AV-Popo
-String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
+    const config = require('./config.json');
 
-module.exports = function AutoCamera(d) {
-	const command = Command(d)
+	// config
+	let data = config,
+		enable = data.enable,
+		setDistance = 0;
+	
+	let playerName = '';
 
-	let enable = config.enable,
-		setDistance = config.defaultDistance
+	// command
+	mod.command.add('cam', {
+		// toggle
+		'$none': () => {
+			enable = !enable;
+			send(`${enable ? 'En' : 'Dis'}abled`);
+		},
+		'$default': (num) => {
+			if (!isNaN(num)) {
+				setCamera(num);
+				send(`Distance set at : ${num}`);
+			}
+			else send(`Invalid argument. usage : cam (num)`);
+		},
+		// set default
+		'add': (num) => {
+			if (!isNaN(num)) {
+				let found = false;
+				for (let i = 0, n = data.characterDefault.length; i < n; i++) {
+					if (data.characterDefault[i].name === playerName) {
+						found = true;
+						data.characterDefault[i].distance = num;
+						break;
+					}
+				}
+				if (!found) {
+					let temp = {
+						name: playerName,
+						distance: num
+					};
+					data.characterDefault.push(temp);
+				}
+				setDistance = num;
+				saveJsonData();
+				send(`Default distance set for "${playerName}" set at ${num}`);
+			}
+			else send(`Invalid argument. usage : cam [add (num)|rm]`);
+		},
+		'rm': () => {
+			for (let i = 0, n = data.characterDefault.length; i < n; i++) {
+				if (data.characterDefault[i].name === playerName) {
+					data.characterDefault.splice(i, 1);
+					saveJsonData();
+					send(`Removed character-specific distance setting for "${playerName}"`);
+					break;
+				}
+			}
+		},
+		'set': (num) => {
+			if (!isNaN(num)) {
+				data.defaultDistance = num;
+				setDistance = num;
+				saveJsonData();
+				send(`Default distance set at ${num}`);
+			}
+			else send(`Invalid argument. usage : cam set (num)`)
+		}
+	});
+
+	// mod.game
+	mod.game.on('enter_game', () => {
+		playerName = mod.game.me.name;
+		setDistance = data.defaultDistance;
+		for (let i = 0, n = data.characterDefault.length; i < n; i++) {
+			if (data.characterDefault[i].name === playerName) {
+				setDistance = data.characterDefault[i].distance;
+				break;
+			}
+		}
+	})
 
 	// code
-	d.hook('S_SPAWN_ME', 'raw', () => {
-		if (enable) setTimeout(() => { setCamera(setDistance) }, 1000)
-	})
+	mod.hook('S_SPAWN_ME', 'raw', () => {
+		if (enable) setTimeout(() => { setCamera(setDistance); }, 1000)
+	});
 
 	// helper
 	function setCamera(distance) {
-		setDistance = distance
-		d.send('S_DUNGEON_CAMERA_SET', 1, { 
+		setDistance = distance;
+		mod.send('S_DUNGEON_CAMERA_SET', 1, {
 			enabled: true,
 			default: setDistance,
 			max: distance
-		})
+		});
 	}
 
-	// command
-	command.add(['camera', 'cam'], (distance) => {
-		// toggle
-		if (!distance) {
-			enable = !enable
-			send(`${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
-		}
-		// set distance
-		else if (!isNaN(distance)) {
-			setCamera(distance)
-			send(`Distance set at ` + `${distance}`.clr('56B4E9'))
-		}
-		else send(`Invalid argument.`.clr('FF0000'))
-	})
-	function send(msg) { command.message(`[auto-camera] : ` + msg) }
+	function saveJsonData() {
+		fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(data));
+	}
+
+	function send(msg) { mod.command.message(`: ` + msg); }
 
 }
